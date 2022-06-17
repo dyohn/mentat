fb0e13bfab438bf33fbb3564febcc9350ec64dc8
Edwin <edilier03@hotmail.com>
2022-06-17 11:08:52 -0400

    Added student controller

---
+++ src/Mentat.UI/Controllers/StudentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mentat.UI.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult StudentView()
+        {
+            return View();
+        }
+    }
+}
