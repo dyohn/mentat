cfae33497449b1d2060e43315945db14efdbf66f
Devin Rucks <blindnives@gmail.com>
2022-06-25 12:39:34 -0400

    Updated with changes from dev branch and updated the mentor controller

--- src/Mentat.UI/Controllers/MentorController.cs
+++ src/Mentat.UI/Controllers/MentorController.cs
@@ -1,12 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Mentat.UI.Controllers
 {
     public class MentorController : Controller
-    {
-        public IActionResult MentorView()
+    {  
+        public IActionResult GradingScriptForm()
         {
             return View();
         }
     }
 }
+
