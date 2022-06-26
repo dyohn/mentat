570940818946b1c0610fdc9054441c0dd579b614
Braden Seidl <81664374+bds18b@users.noreply.github.com>
2022-06-26 02:54:49 -0400

    Create GradingScriptFormController.cs

---
+++ src/Mentat.UI/Controllers/GradingScriptFormController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mentat.UI.Controllers
+{
+    public class GradingScriptFormController : Controller
+    {
+        public IActionResult GradingScriptForm()
+        {
+            //doesnt currently workk
+            string test = this.Request.Form["language"];          
+            Console.WriteLine(test);
+            return View();
+        }
+    }
+}
+
