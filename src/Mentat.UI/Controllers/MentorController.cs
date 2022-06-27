b614a3000e635d2078935b42627f02ba84aaf91b
bds18b <bds18b@my.fsu.edu>
2022-06-26 23:36:16 -0400

    got the form input working, currently just prints the form input in console and redirects to home page

--- src/Mentat.UI/Controllers/MentorController.cs
+++ src/Mentat.UI/Controllers/MentorController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Mentat.UI.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -13,6 +14,23 @@ namespace Mentat.UI.Controllers
         public IActionResult GradingScriptForm()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult GetFormData(string language, string file_names, string exec_name, string duration, string diff_cmd)
+        {
+            GradingScriptFormModel form = new GradingScriptFormModel();
+            form.language = language;
+            form.file_names = file_names;
+            form.exec_name = exec_name;
+            form.duration = duration;
+            form.diff_cmd = diff_cmd;
+            Console.WriteLine(form.language);
+            Console.WriteLine(form.file_names);
+            Console.WriteLine(form.exec_name);
+            Console.WriteLine(form.duration);
+            Console.WriteLine(form.diff_cmd);
+            return View("/Views/Home/Index.cshtml");
         }
     }
 }
