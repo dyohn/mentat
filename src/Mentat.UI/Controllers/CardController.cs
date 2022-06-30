80c329e785adc21281e0f0edc6a6c5c8ab77ed7f
Cole Winfield <cw19t@my.fsu.edu>
2022-06-29 18:51:53 -0700

    Refactor CardController for succinctness

--- src/Mentat.UI/Controllers/CardController.cs
+++ src/Mentat.UI/Controllers/CardController.cs
@@ -6,6 +6,7 @@ using MongoDB.Driver;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Mentat.UI.Services;
+using Mentat.UI.Models;
 
 namespace Mentat.UI.Controllers
 {
@@ -16,23 +17,13 @@ namespace Mentat.UI.Controllers
         {
             this.cardService = cardService;
         }
+
+        [HttpGet]  
         // GET: CardController
         public ActionResult Index()
         {
-            Models.MongoHelper.ConnectToMongoService();
-            Models.MongoHelper.FlashCards =
-                           Models.MongoHelper.database.GetCollection<Models.Card>("FlashCards");
-
             var cards = cardService.Get();
-
-            var filter = Builders<Models.Card>.Filter.Ne("_id", "");
-            var result = Models.MongoHelper.FlashCards.Find(filter).ToList();
-
-            //var result = cardService.Get().Find(filter).ToList();
-
-            
-
-            return View(result);
+            return View(cards);
         }
 
         // GET: CardController/Details/5
@@ -48,7 +39,7 @@ namespace Mentat.UI.Controllers
             return View(result);
         }
 
-        // GET: CardController/Create
+        // POST: CardController/Create
         public ActionResult Create()
         {
             return View();
@@ -60,18 +51,14 @@ namespace Mentat.UI.Controllers
         public ActionResult Create(IFormCollection collection)
         {
             try
-            {
-                Models.MongoHelper.ConnectToMongoService();
-                Models.MongoHelper.FlashCards =
-                               Models.MongoHelper.database.GetCollection<Models.Card>("FlashCards");
-                
+            {                
                 object id = GenRandomId(24);
-
-                Models.MongoHelper.FlashCards.InsertOneAsync(new Models.Card
+                cardService.Create(new Models.Card
                 {
                   _id = id,
                   notes = collection["notes"]
-                });  
+                });
+                
                 return RedirectToAction(nameof(Index));
             }
             catch(Exception e)
