24995ffcc5f9d75cdc837c4e083e7567d441dc11
Claudio Osorio <cbo20h@fsu.com>
2022-07-16 16:08:58 -0400

    Form Validation
    Re-formatted the AddAssignment form for clarity.
    Created a new view "SubmitForm". When the form is validated the data is sent to the new "SubmitForm" view. This can be changed later to accommodate for the necesities of the other areas.
    Implemented basic form validation by ensuring the fields are not left blank. Text in the fields cannot be longer than specified.

--- src/Mentat.UI/Controllers/MentorController.cs
+++ src/Mentat.UI/Controllers/MentorController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.View;
 
 namespace Mentat.UI.Controllers
 {
@@ -10,8 +13,10 @@ namespace Mentat.UI.Controllers
             return View();
         }
 
-        public IActionResult AddAssignment()
+
+        public IActionResult AddAssignment(Models.Assignment assignment)
         {
+ 
             return View();
         }
 
@@ -19,12 +24,30 @@ namespace Mentat.UI.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult SubmitForm(Models.Assignment assignment)
         {
+
+            //DEMO OF HOW VALUES ARE STORED
             var mentorName = assignment.MentorName;
             var assignmentName = assignment.AssignmentName;
             var assignmentType = assignment.AssignmentType;
             var sampleExecutableName = assignment.SampleExecutableName;
             var testFileNames = assignment.TestFileNames;
+
+            // If form is correct then moves on to the next view 
+            // otherwise view is reloaded and user must retry filling the form
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("SendData", assignment);
+            }
+ 
             return View();
         }
+
+        // On Form Validation Success Go To This View
+        public IActionResult SendData(Models.Assignment assignment)
+        {
+            return View("SubmitForm");
+        }
+
+
     }
 }
--- src/Mentat.UI/Models/Assignment.cs
+++ src/Mentat.UI/Models/Assignment.cs
@@ -8,16 +8,51 @@ namespace Mentat.UI.Models
 {
     public class Assignment
     {
+        // ASSIGNMENT ID
+        [Required(AllowEmptyStrings = false)]
+        [Display(Name = "Assignment Id")]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Assignment Id must be digits 0-9 only")]
         public int Id { get; set; }
-        [Required]
+
+        // MENTOR NAME
+        [Required(AllowEmptyStrings = false)]
+        [Display(Name = "Mentor Name")]
+        [StringLength(80, ErrorMessage = "Mentor name may not exceed 80 characters")]
+        [RegularExpression(".*\\S.*", ErrorMessage = "Mentor name cannot be blank")]
         public string MentorName { get; set; }
-        [Required]
+
+        // ASSIGNMENT NAME
+        [Required(AllowEmptyStrings = false)]
+        [Display(Name = "Assignment Name")]
+        [StringLength(30, ErrorMessage = "Assignment name may not exceed 30 characters")]
+        [RegularExpression(".*\\S.*", ErrorMessage = "Assignment name cannot be blank")]
         public string AssignmentName { get; set; }
-        [Required]
+
+        // ASSIGNMENT TYPE
+        [Required(AllowEmptyStrings = false)]
+        [Display(Name = "Assignment Type")]
         public string AssignmentType { get; set; }
+
+        // SAMPLE EXECUTABLE FILE NAME
         [Required]
+        [Display(Name = "Sample File Name")]
+        [StringLength(255, ErrorMessage = "Sample file name may not exceed 255 characters")]
+        [RegularExpression(".*\\S.*", ErrorMessage = "Sample executable cannot be blank")]
         public string SampleExecutableName { get; set; }
-        [Required]
-        public string TestFileNames { get; set; }
+
+        // TEST FILES NAME
+        [Required(AllowEmptyStrings = false)]
+        [Display(Name = "Test Files Names")]
+        [RegularExpression(".*\\S.*", ErrorMessage = "Test File field cannot be blank. Enter a single file name by line")]
+        public string[] TestFileNames { get; set; }
+
+
+
+
     }
+
+
+
+
+
 }
