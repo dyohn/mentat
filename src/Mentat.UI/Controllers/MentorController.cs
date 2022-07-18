986aa0e58b97338249387fc84ee17af7c2c57904
Claudio Osorio <cbo20h@fsu.com>
2022-07-17 21:29:19 -0400

    Enhanced Form Validation and more
    Added more contraints to the form validation to check for .c or .cpp file names
    Fixed some typos
    Remove comments and code used for demostration
    Refactored the success view used after the form is submitted. Now the view has the correct URL and provides the correct feedback to the user.

--- src/Mentat.UI/Controllers/MentorController.cs
+++ src/Mentat.UI/Controllers/MentorController.cs
@@ -1,8 +1,5 @@
-﻿using System;
+﻿using Mentat.UI.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Http;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.View;
 
 namespace Mentat.UI.Controllers
 {
@@ -13,41 +10,28 @@ namespace Mentat.UI.Controllers
             return View();
         }
 
-
-        public IActionResult AddAssignment(Models.Assignment assignment)
+        public IActionResult AddAssignment(Assignment assignment)
         {
- 
             return View();
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult SubmitForm(Models.Assignment assignment)
+        public ActionResult SubmitForm(Assignment assignment)
         {
-
-            //DEMO OF HOW VALUES ARE STORED
-            var mentorName = assignment.MentorName;
-            var assignmentName = assignment.AssignmentName;
-            var assignmentType = assignment.AssignmentType;
-            var sampleExecutableName = assignment.SampleExecutableName;
-            var testFileNames = assignment.TestFileNames;
-
             // If form is correct then moves on to the next view 
             // otherwise view is reloaded and user must retry filling the form
             if (ModelState.IsValid)
             {
-                return RedirectToAction("SendData", assignment);
+                // On Form Validation Success Go To This View
+                return RedirectToAction("Success");
             }
- 
+            return View("AddAssignment");
+        }
+
+        public ActionResult Success(Assignment assignment)
+        {
             return View();
         }
-
-        // On Form Validation Success Go To This View
-        public IActionResult SendData(Models.Assignment assignment)
-        {
-            return View("SubmitForm");
-        }
-
-
     }
 }
--- src/Mentat.UI/Models/Assignment.cs
+++ src/Mentat.UI/Models/Assignment.cs
@@ -1,58 +1,106 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.IO;
 using System.Linq;
 using System.Web;
+using Mentat.UI.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore;
 
 namespace Mentat.UI.Models
 {
+    public class FileExtensionCheckAttribute : ValidationAttribute
+    {
+        public FileExtensionCheckAttribute()
+        {
+            Assignment assignment = new Assignment();
+            if (assignment.AssignmentType != null)
+            {
+                this.FileType = assignment.AssignmentType;
+            }
+            else
+            {
+                this.FileType = "None";
+            }
+        }
+
+        public string FileType { set; get; }
+    public string GetErrorMessage()
+        {
+            if (this.FileType.ToLower() == "c")
+            {
+                return $"The file name must end with the selected suffix \".c\"";
+            }
+            else if (this.FileType.ToLower() == "c++")
+            {
+                return $"The file name must end with the selected suffix \".cpp\"";
+            }
+            return $"Your file extension must match the type you selected. [Eg: file.c or file.cpp]";
+        }
+
+        protected override ValidationResult? IsValid(
+            object? value, ValidationContext validationContext)
+        {
+            var fileName = ((string)value!);
+            var fileType = (Assignment)validationContext.ObjectInstance;
+
+            try
+            {
+                if (fileType.AssignmentType == "c" &&
+                    fileName.Substring(fileName.Length - 2).ToLower() == ".c")
+                {
+                    return ValidationResult.Success;
+
+                }
+                else if (fileType.AssignmentType == "c++" &&
+                         fileName.Substring(fileName.Length - 4).ToLower() == ".cpp")
+                {
+                    return ValidationResult.Success;
+                }
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                return new ValidationResult(GetErrorMessage());
+            }
+            return new ValidationResult(GetErrorMessage());
+        }
+    }
     public class Assignment
     {
-        // ASSIGNMENT ID
         [Required(AllowEmptyStrings = false)]
         [Display(Name = "Assignment Id")]
         [RegularExpression("^[0-9]*$", ErrorMessage = "Assignment Id must be digits 0-9 only")]
         public int Id { get; set; }
 
-        // MENTOR NAME
         [Required(AllowEmptyStrings = false)]
         [Display(Name = "Mentor Name")]
         [StringLength(80, ErrorMessage = "Mentor name may not exceed 80 characters")]
         [RegularExpression(".*\\S.*", ErrorMessage = "Mentor name cannot be blank")]
         public string MentorName { get; set; }
 
-        // ASSIGNMENT NAME
         [Required(AllowEmptyStrings = false)]
         [Display(Name = "Assignment Name")]
         [StringLength(30, ErrorMessage = "Assignment name may not exceed 30 characters")]
         [RegularExpression(".*\\S.*", ErrorMessage = "Assignment name cannot be blank")]
         public string AssignmentName { get; set; }
 
-        // ASSIGNMENT TYPE
         [Required(AllowEmptyStrings = false)]
         [Display(Name = "Assignment Type")]
         public string AssignmentType { get; set; }
 
-        // SAMPLE EXECUTABLE FILE NAME
         [Required]
         [Display(Name = "Sample File Name")]
         [StringLength(255, ErrorMessage = "Sample file name may not exceed 255 characters")]
         [RegularExpression(".*\\S.*", ErrorMessage = "Sample executable cannot be blank")]
+        [FileExtensionCheck]
         public string SampleExecutableName { get; set; }
 
-        // TEST FILES NAME
         [Required(AllowEmptyStrings = false)]
-        [Display(Name = "Test Files Names")]
-        [RegularExpression(".*\\S.*", ErrorMessage = "Test File field cannot be blank. Enter a single file name by line")]
-        public string[] TestFileNames { get; set; }
-
-
-
-
+        [Display(Name = "Test File Name")]
+        [RegularExpression(".*\\S.*", ErrorMessage = "Test file field cannot be blank")]
+        [FileExtensionCheck]
+        public string TestFileName { get; set; }
     }
-
-
-
-
-
 }
