74a237a0b59766a73c705843f18abea8730b17a2
BuildTools <unconfigured@null.spigotmc.org>
2023-03-06 18:49:39 -0500

    Implemented test controllers for the register & login buttons on the home page that pull up a page with the text "test".

---
+++ src/Mentat.UI/Controllers/LoginController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mentat.UI.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        [HttpGet]
+        public string Index()
+        {
+            return "test";
+        }
+    }
+}
---
+++ src/Mentat.UI/Controllers/RegisterController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mentat.UI.Controllers
+{
+    public class RegisterController : Controller
+    {
+
+        [HttpGet]
+        public string Index()
+        {
+            return "test";
+        }
+    }
+}
