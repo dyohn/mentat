35cc20f585c4dc31901c086ac18a6df021ce9f0c
Durenir <89751699+Durenir@users.noreply.github.com>
2023-03-20 13:00:16 -0500

    Add comments to BashColor.cs

--- src/Mentat.Domain/BashColor.cs
+++ src/Mentat.Domain/BashColor.cs
@@ -7,18 +7,25 @@ using System.Threading.Tasks;
 
 namespace Mentat.Domain
 {
+    /// <summary>
+    /// A convenient class for generating bash color decorators that can be used to wrap a string.
+    /// </summary>
     public class BashColor
     {
+        /// <value>Holds the string containing the color formatting.</value>
         public String Color { get; private set; }
+        /// <value>Holds the escape sequence used to start color formatting.</value>
         private const String _escape = "\\e[";
+        /// <value>Holds the string for ending a strings formatting.</value>
         public static String End = "\\e[0m";
 
-
+        /// <value>Text modifiers not involving colors.</value>
         public enum TextModifier
         {
             Reset, Bold, Faint, Italics, Underline
         }
 
+        /// <value>Values for changing the color of the actual text.</value>
         public enum TextColor
         {
             Black = 30,
@@ -39,6 +46,7 @@ namespace Mentat.Domain
             White = 97
         }
 
+        /// <value>Values for changing the color of the background of text (highlighting).</value>
         public enum BackgroundColor
         {
             Black = 40,
@@ -59,11 +67,19 @@ namespace Mentat.Domain
             White = 107
         }
 
+        /// <summary>
+        /// Constructor that generates the decorator string that will be held by the object.
+        /// </summary>
+        /// <param name="text">The color of the text.</param>
+        /// <param name="background">The color of the background.</param>
+        /// <param name="modifier">Modifier to apply to the style of the text.</param>
         public BashColor(BashColor.TextColor? text, BashColor.BackgroundColor? background, BashColor.TextModifier? modifier)
         {
             Color += _escape;
+            // Check if any params were supplied. If not, generate the end token.
             if (text.HasValue || background.HasValue || modifier.HasValue)
             {
+                // Is a modifier being applied? If so, append it. If not, append the default 0.
                 if (modifier.HasValue)
                 {
                     Color += ((int)modifier.Value);
@@ -72,10 +88,12 @@ namespace Mentat.Domain
                 {
                     Color += ("0");
                 }
+                // Apply a delimiter if there are any proceeding values.
                 if (background.HasValue || text.HasValue)
                 {
                     Color += (";");
                 }
+                // Append the background color if applicable. If needed, append a delimiter afterwards.
                 if (background.HasValue)
                 {
                     Color += (int)background.Value;
@@ -84,6 +102,7 @@ namespace Mentat.Domain
                         Color += ";";
                     }
                 }
+                // Append the text color if applicable.
                 if (text.HasValue)
                 {
                     Color += (int)text.Value;
