d96404fd2aa902a528dbf0e581291d7586d1cec6
Ell-Gee <124177032+Ell-Gee@users.noreply.github.com>
2023-03-22 13:39:46 -0500

    Add beginning/skeleton of Register functionality
    Register drop-down redirects to Register page with VM and Controller. Logs input to console and returns user to Mentor or Student page, depending on registry "type"

--- src/Mentat.UI/Controllers/RegisterController.cs
+++ src/Mentat.UI/Controllers/RegisterController.cs
@@ -1,15 +1,50 @@
 ﻿using System;
 using Microsoft.AspNetCore.Mvc;
+using Mentat.UI.ViewModels;
+using Mentat.UI.Controllers;
+using Microsoft.Extensions.Logging;
+
 
 namespace Mentat.UI.Controllers
 {
     public class RegisterController : Controller
     {
+        private readonly ILogger<RegisterController> _logger;
+
+
+        public RegisterController(ILogger<RegisterController> logger)
+        {
+            _logger = logger;
+        }
 
         [HttpGet]
-        public string Index()
+        public IActionResult Register()
         {
-            return "test";
+            // create + return new instance of VM:
+            var model = new RegisterViewModel();
+            return View(model);
+        }
+
+
+        [HttpPost]
+        public IActionResult Register(RegisterViewModel model)
+        {
+            
+            _logger.LogInformation("Name: {nm}, \nPW: {pw}, \nType: {type}", model.UserName, model.Password,
+                model.UserType);
+
+
+            if (model.UserType == 0)
+            {
+                return RedirectToAction("Index", "Student");
+            }
+
+            else if (model.UserType == 1)
+            {
+                return RedirectToAction("Index", "Mentor");
+            }
+
+            return RedirectToAction("Index", "Home"); 
         }
     }
 }
---
+++ src/Mentat.UI/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Mentat.UI.ViewModels;
+
+
+public class RegisterViewModel
+{
+    public string UserName { get; set; }
+    
+    public string Password { get; set; }
+
+    public int UserType { get; set; } // 0 for students, 1 for mentors
+
+}
