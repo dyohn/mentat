5b061f1f601cecb9b2563e8c6d3753ad9f225039
Durenir <89751699+Durenir@users.noreply.github.com>
2023-04-07 11:25:03 -0500

    Update download

---
+++ src/Mentat.Domain/Bash/FileManagerService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mentat.Domain.Interfaces;
+
+namespace Mentat.Domain.Bash
+{
+    internal class FileManagerService : IFileManagerService
+    {
+        public void SaveScript(byte[] script, string scriptName)
+        {
+            try
+            {
+                using (var fs = new FileStream(scriptName, FileMode.Create, FileAccess.Write))
+                {
+                    fs.Write(script, 0, script.Length);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception caught in process: {0}", ex);
+            }
+        }
+    }
+}
--- src/Mentat.UI/Controllers/MentorController.cs
+++ src/Mentat.UI/Controllers/MentorController.cs
@@ -7,6 +7,7 @@ using Mentat.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Linq;
+using Mentat.Domain.Interfaces;
 
 namespace Mentat.UI.Controllers
 {
@@ -26,18 +27,27 @@ namespace Mentat.UI.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult SubmitForm(Assignment assignment)
         {
-            string sampleExecutableName = assignment.SampleExecutableName;
-            string assignmentType = assignment.AssignmentType;
-            string concatenatedTestFileNames = assignment.TestFiles;
-            List<string> testFileNames = concatenatedTestFileNames.Split(',').ToList();
-            int id = assignment.Id;
-            string language = assignment.AssignmentType.ToString();
             // If form is correct then moves on to the next view 
             // otherwise view is reloaded and user must retry filling the form
+
             if (ModelState.IsValid)
             {
+                string language = assignment.AssignmentType.ToString();
+                string sampleExecutableName = assignment.SampleExecutableName;
+                string assignmentType = assignment.AssignmentType;
+                string concatenatedTestFileNames = assignment.TestFiles;
+                List<string> testFileNames = concatenatedTestFileNames.Split(',').ToList();
+                int id = assignment.Id;
+                bool colorText = assignment.ColorText;
+                bool highlightText = assignment.HighlightText;
+                bool applyTextModifiers = assignment.ApplyTextModifiers;
+                BashTestConfig configs = new BashTestConfig(language, testFileNames, sampleExecutableName, 10, "diff", colorText, highlightText, applyTextModifiers);
+                BashTestDriver bashDriver = new BashTestDriver(new FileManagerService());
+                bashDriver.Configure(configs);
+                FileInfo fileInfo = bashDriver.Build();
                 // On Form Validation Success Go To This View
-                return RedirectToAction("Success");
+                TempData["fileInfo"] = fileInfo.FullName;
+                return RedirectToAction("download");
             }
             return View("AddAssignment");
         }
@@ -52,16 +62,18 @@ namespace Mentat.UI.Controllers
         // *             Download method              *
         // ********************************************
         // Download a file to the user's local machine.
-        public IActionResult Download()
+        public ActionResult Download()
         {
-            string content = "Hello, World!";
-            string fileName = "file.txt";
+            //string content = "Hello, World!";
+            //string fileName = "file.txt";
+            string path = Convert.ToString(TempData["fileInfo"]);
+            string content = System.IO.File.ReadAllText(path);
 
             var stream = new MemoryStream(Encoding.UTF8.GetBytes(content));
 
             return new FileStreamResult(stream, "text/plain")
             {
-                FileDownloadName = fileName
+                FileDownloadName = path
             };
         }
     }
