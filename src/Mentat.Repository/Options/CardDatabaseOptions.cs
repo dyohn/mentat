763638d0c346ee3e3669f4f8d3a54fa491a7354e
angrychai <72465107+angrychai@users.noreply.github.com>
2023-04-08 15:59:59 -0400

    Implement Options pattern for Identity Database settings

--- src/Mentat.Repository/Options/CardDatabaseOptions.cs
+++ src/Mentat.Repository/Options/CardDatabaseOptions.cs
@@ -2,7 +2,7 @@
 namespace Mentat.Repository.Options
 {
     /// <summary>
-    /// Tightly coupled class for containing database connection options.
+    /// Tightly coupled class for containing Card Database connection options.
     /// Accessed in CardServices via IOptionsMonitor pattern.
     /// </summary>
     public class CardDatabaseOptions
