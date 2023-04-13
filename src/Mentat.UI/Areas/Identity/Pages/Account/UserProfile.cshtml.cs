9792b14ea8815f66c3cc216f981a655a38eed6aa
Ell-Gee <124177032+Ell-Gee@users.noreply.github.com>
2023-04-13 14:37:50 -0500

    UserProfile updates user info; plaintext toggles added to PW fields

--- src/Mentat.UI/Areas/Identity/Pages/Account/UserProfile.cshtml.cs
+++ src/Mentat.UI/Areas/Identity/Pages/Account/UserProfile.cshtml.cs
@@ -6,6 +6,7 @@ using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Mentat.UI.Areas.Identity.Data;
+using Mentat.UI.Controllers;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
@@ -15,7 +16,6 @@ using Microsoft.Extensions.Logging;
 
 namespace Mentat.UI.Areas.Identity.Pages.Account
 {
-    
     public class UserProfileModel : PageModel
     {
         private readonly UserManager<MentatUser> _userManager;
@@ -28,61 +28,38 @@ namespace Mentat.UI.Areas.Identity.Pages.Account
             Input = new InputModel();
         }
 
-        public InputModel Input { get; set; }
+        [BindProperty] public InputModel Input { get; set; }
 
-        public string returnUrl{ get; set; }
-
-        [TempData]
-        public string ErrorMessage { get; set; }
-        
-        [Required]
-        public string Username { get; set; }
-        [Required]
-        public string Email { get; set; }
-
-        public string CurrentPassword{get;set;}
 
         public class InputModel
         {
             [Required]
-            [Display(Name = "User Name")]
-            [BindProperty]
-            public string UserName {get; set;}
+            [Display(Name = "UserName")]
+            public string UserName { get; set; }
 
             [Required]
             [EmailAddress]
             [Display(Name = "Email")]
-            [BindProperty]
-            public string Email {get; set;}
+            public string Email { get; set; }
 
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.",
-                MinimumLength = 6)]
+            [Required]
             [DataType(DataType.Password)]
-            [Display(Name = "Current Password")]
-            [BindProperty]
-            public string Password {get;set;}
-            
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.",
-                MinimumLength = 6)]
+            [Display(Name = "Confirm current password")]
+            public string Password { get; set; }
+
             [DataType(DataType.Password)]
-            [Display(Name = "New Password")]
-            [BindProperty]
+            [Display(Name = "Change password")]
             public string NewPassword { get; set; }
-            
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.",
-                MinimumLength = 6)]
-            [DataType(DataType.Password)]
-            [Display(Name = "Confirm New Password")]
-            [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
-            [BindProperty]
-            public string ConfirmPassword { get; set; }
 
-             [Required]public string UserType { get; }
+            [DataType(DataType.Password)]
+            [Display(Name = "Confirm new password")]
+            [Compare("NewPassword", ErrorMessage = "The password and confirmation password do not match.")]
+            public string ConfirmPassword { get; set; }
         }
 
         public async Task<IActionResult> OnGetAsync()
         {
-            if(!ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
                 ModelState.AddModelError(string.Empty, "Invalid use profile access attempt.");
                 return Page();
@@ -95,6 +72,7 @@ namespace Mentat.UI.Areas.Identity.Pages.Account
                 return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
             }
 
+            // Assign the Input model before returning the page
             Input.UserName = user.UserName;
             Input.Email = user.Email;
 
@@ -105,92 +83,66 @@ namespace Mentat.UI.Areas.Identity.Pages.Account
                 return Page();
             } */
 
-
-
             return Page();
         }
 
         public async Task<IActionResult> OnPostAsync()
         {
-            var user = await _userManager.GetUserAsync(User);
-            if (user == null || !User.Identity.IsAuthenticated)
-            {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
-            }
-
-            // Assign the Input model before returning the page
-            Input = new InputModel
-            {
-                UserName = user.UserName,
-                Email = user.Email
-            };
-
-            /* Currently we do not have it setup so that emails are verified. Thus this part is not needed yet.
-            
-            if(!user.EmailConfirmed)
-            {
-                ModelState.AddModelError(string.Empty,"You must have a confirmed email to update your profile.");
-                return Page();
-            }
- */
             if (!ModelState.IsValid)
             {
-                
                 return Page();
             }
 
-            var email = await _userManager.GetEmailAsync(user); 
-            if (Input.Email != email)
+            var user = await _userManager.GetUserAsync(User);
+            var passwordCorrect = await _userManager.CheckPasswordAsync(user, Input.Password);
+            if (!passwordCorrect)
             {
-                var setEmailResult = await _userManager.SetEmailAsync(user, Input.Email);
-                if (!setEmailResult.Succeeded)
-                {
-                    var userId = await _userManager.GetUserIdAsync(user);
-                    throw new InvalidOperationException($"Unexpected error occurred setting email for user with ID '{userId}'.");
-                }
+                ModelState.AddModelError(string.Empty, "Incorrect password.");
+                return Page();
             }
 
-            var username = await _userManager.GetUserNameAsync(user);
-            if (Input.UserName != username)
+            if (Input.UserName != user.UserName)
             {
                 var setUserNameResult = await _userManager.SetUserNameAsync(user, Input.UserName);
                 if (!setUserNameResult.Succeeded)
                 {
-                    var userId = await _userManager.GetUserIdAsync(user);
-                    throw new InvalidOperationException($"Unexpected error occurred setting user name for user with ID '{userId}'.");
+                    ModelState.AddModelError(string.Empty, "UserName not updated.");
                 }
             }
-            if (!string.IsNullOrEmpty(Input.NewPassword))
+
+            if (Input.Email != user.Email)
             {
-                var changePasswordResult = await _userManager.ChangePasswordAsync(user, Input.Password, Input.NewPassword);
+                var setEmailResult = await _userManager.SetEmailAsync(user, Input.Email);
+                if (!setEmailResult.Succeeded)
+                {
+                    ModelState.AddModelError(string.Empty, "Email address not updated.");
+                }
+            }
+
+            if (!string.IsNullOrEmpty(Input.NewPassword) && Input.NewPassword == Input.ConfirmPassword)
+            {
+                var changePasswordResult =
+                    await _userManager.ChangePasswordAsync(user, Input.Password, Input.NewPassword);
                 if (!changePasswordResult.Succeeded)
                 {
                     foreach (var error in changePasswordResult.Errors)
                     {
                         ModelState.AddModelError(string.Empty, error.Description);
                     }
+
                     return Page();
                 }
             }
 
-            
-            var result = await _userManager.UpdateAsync(user);
-            if (result.Succeeded)
-            {
-                await _signInManager.RefreshSignInAsync(user);
-                return RedirectToPage("UserProfile", new { returnUrl = returnUrl });
-            }
+            var result = await _userManager.UpdateAsync(user); // update user
+            await _signInManager.RefreshSignInAsync(user); // update userName in _LoginPartial
 
             foreach (var error in result.Errors)
             {
                 ModelState.AddModelError(string.Empty, error.Description);
             }
 
-            // Assign the Input model before returning the page
-            Input.UserName = user.UserName;
-            Input.Email = user.Email;
-
-            return Page();
+            return Redirect("~/"); // Return user to root page
         }
     }
 }
