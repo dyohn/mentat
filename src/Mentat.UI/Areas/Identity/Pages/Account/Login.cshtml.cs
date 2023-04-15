bdec1c22f8575c069cdce323440e3f52b8a328e9
Ell-Gee <124177032+Ell-Gee@users.noreply.github.com>
2023-04-15 15:38:04 -0500

    Update look + feel of Login and Register forms
    Add password toggles; mimic style of UserProfile.cshtml

--- src/Mentat.UI/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ src/Mentat.UI/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -51,7 +51,7 @@ namespace Mentat.UI.Areas.Identity.Pages.Account
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
-            [Display(Name = "Remember me?")]
+            [Display(Name = "Remember me")]
             public bool RememberMe { get; set; }
         }
 
