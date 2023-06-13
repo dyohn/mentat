30404661c41026396004a004838d69c55cec0b9d
wfbscarmona <76072605+codyccarmona@users.noreply.github.com>
2023-06-13 11:04:38 -0400

    define user role type constants

---
+++ src/Mentat.UI/Areas/Identity/Data/MentatUserRoleTypes.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Mentat.UI.Areas.Identity.Data
+{
+    public static class MentatUserRolesTypes
+    {
+        public static string Mentor => "Mentor";
+
+        public static string Student => "Student";
+    }
+}
