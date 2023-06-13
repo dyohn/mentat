39ff565caba6f351d64603cf64c041f65bbf6fd4
wfbscarmona <76072605+codyccarmona@users.noreply.github.com>
2023-06-13 16:30:53 -0400

    update MentatUserRoleTypes.cs
    remove properties. empty class sufficient for authorization

--- src/Mentat.UI/Areas/Identity/Data/MentatUserRoleTypes.cs
+++ src/Mentat.UI/Areas/Identity/Data/MentatUserRoleTypes.cs
@@ -3,41 +3,9 @@ using System.Linq;
 
 namespace Mentat.UI.Areas.Identity.Data
 {
-    public static class MentatUserRolesTypes
+    public class MentatUserRolesType
     {
-        static ICollection<string> _roles;
-        static ICollection<string> _privilegedRoles;
-        
-        static MentatUserRolesTypes()
-        {
-            _roles = new HashSet<string>()
-            {
-                MentatUserRolesTypes.Student
-            };
-
-            _privilegedRoles = new HashSet<string>()
-            {
-                MentatUserRolesTypes.Mentor
-            };
-        }
-
-        public static ICollection<string> AllRoles()
-        {            
-            return _roles.Concat(_privilegedRoles).ToList<string>();
-        }
-
-        public static ICollection<string> PrivilegedRoles()
-        {
-            return _privilegedRoles.ToList<string>();
-        }
-
-        public static bool IsPrivilegedUserRole(string userRolesType)
-        {
-            return _privilegedRoles.Contains(userRolesType);
-        }
-
-        public static string Mentor => "Mentor";
-
-        public static string Student => "Student";
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
