201f12570264dc26ed6d06b294b97f30f3c55d50
wfbscarmona <76072605+codyccarmona@users.noreply.github.com>
2023-06-13 10:38:49 -0400

    create page authorization filter

---
+++ src/Mentat.UI/Filters/UserTypeAuthorizationFilter.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Mentat.UI.Filters
+{
+    public class UserTypeAuthorizationFilter : IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
