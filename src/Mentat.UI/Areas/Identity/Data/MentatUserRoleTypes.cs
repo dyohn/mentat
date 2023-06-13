6e630baa85c95d92533d959f8aa73076bcc3d6a4
wfbscarmona <76072605+codyccarmona@users.noreply.github.com>
2023-06-13 11:18:15 -0400

    Update MentatUserRoleTypes.cs
    define user role type constants
    
    add user role type functionality

--- src/Mentat.UI/Areas/Identity/Data/MentatUserRoleTypes.cs
+++ src/Mentat.UI/Areas/Identity/Data/MentatUserRoleTypes.cs
@@ -1,9 +1,41 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Mentat.UI.Areas.Identity.Data
 {
     public static class MentatUserRolesTypes
     {
+        static ICollection<string> _roles;
+        static ICollection<string> _privilegedRoles;
+        
+        static MentatUserRolesTypes()
+        {
+            _roles = new HashSet<string>()
+            {
+                MentatUserRolesTypes.Student
+            };
+
+            _privilegedRoles = new HashSet<string>()
+            {
+                MentatUserRolesTypes.Mentor
+            };
+        }
+
+        public static ICollection<string> AllRoles()
+        {            
+            return _roles.Concat(_privilegedRoles).ToList<string>();
+        }
+
+        public static ICollection<string> PrivilegedRoles()
+        {
+            return _privilegedRoles.ToList<string>();
+        }
+
+        public static bool IsPrivilegedUserRole(string userRolesType)
+        {
+            return _privilegedRoles.Contains(userRolesType);
+        }
+
         public static string Mentor => "Mentor";
 
         public static string Student => "Student";
