de1855246b123691158a8ac2cb9484e3183abcc6
wfbscarmona <codycarmona@gmail.com>
2023-06-16 15:47:57 -0400

    Define user type authorize requirement
    checks if user has role contained in the list of required types

---
+++ src/Mentat.UI/Areas/Identity/UserRoleTypeRequirement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Mentat.UI.Areas.Identity
+{
+    public class UserRoleTypeRequirement : IAuthorizationRequirement
+    {
+        public IEnumerable<string> RequiredTypes { get; set; }
+
+        public UserRoleTypeRequirement(params string []type)
+        {
+            RequiredTypes = type.ToList<string>();
+        }
+    }
+}
