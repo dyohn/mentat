6f9b6f0e3a8cdaec7c68deda6b9f5ba29163c81f
Davis Phillips <dep13@fsu.edu>
2023-07-16 20:18:09 -0400

    -Added isPrivate and Owner on the delete page -Added a new Delete Function in the CardController which needs a user passed to it to verify the user making the change is the owner of the card.

--- src/Mentat.UI/Controllers/CardController.cs
+++ src/Mentat.UI/Controllers/CardController.cs
@@ -74,10 +74,27 @@ namespace Mentat.UI.Controllers
             }
         }
 
+        // DEPRICATED - Please use the Delete function below with a user parameter
         // GET: CardController/Delete/5
         public ActionResult Delete(string id)
         {
             var card = _cardService.GetCard(id);
+            if (card == null)
+            {
+                return NotFound($"Card with ID = {id} not found");
+            }
+
+            return View(new CardViewModel(card));
+        }
+
+        // GET: CardController/Delete/5
+        public ActionResult Delete(string id, string user)
+        {
+            var card = _cardService.GetCard(id);
+            if (card.Owner != user)
+            {
+                return NotFound($"{user} is not the owner of this card and cannot delete it.");
+            }
             if (card == null)
             {
                 return NotFound($"Card with ID = {id} not found");
